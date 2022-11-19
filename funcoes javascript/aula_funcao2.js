3e99b592f2e3ebac58cbb217907e297eb241bc03
Wendell Gabriel Fernandes Barros <102330689+mrwendellbarros@users.noreply.github.com>
2022-11-19 09:28:13 -0300

    funcoes

---
+++ funcoes javascript/aula_funcao2.js
@@ -0,0 +1,15 @@
+function escrevaMeuNome(nome){
+  return `Meu nome é ${nome}`
+}
+
+escrevaMeuNome('Wendell')
+
+function verificarIdade(idade){
+  if (idade > 18){
+    console.log('Maior de 18')
+  } else {
+    console.log('Menor de 18')
+  }
+}
+
+verificarIdade(21)
---
+++ funcoes javascript/juros_com_funcoes.js
@@ -0,0 +1,20 @@
+function aplicarDesconto(valor, desconto){
+  return valor - (valor * (desconto / 100))
+}
+
+function aplicarJuros(valor, juros){
+  return valor + (valor * (juros / 100))
+}
+
+const precoEtiqueta = 100
+const formaDePagamento = 1
+
+if (formaDePagamento === 1) {
+  console.log(`O produto saiu por ${aplicarDesconto(precoEtiqueta, 10)}`)
+} else if (formaDePagamento === 2) {
+  console.log(aplicarDesconto(precoEtiqueta,15))
+} else if (formaDePagamento === 3) {
+  console.log(precoEtiqueta)
+} else {
+  console.log(aplicarJuros(precoEtiqueta, 10))
+}
